65d1aa18a2844ff8abd1ead615b565088f65d059
GuilhermeW3ber <brococaxi@hotmai.com>
2023-03-13 23:15:48 -0300

    First Commit

--- new_scraper.py
+++ new_scraper.py
@@ -5,11 +5,9 @@ import requests
 import time
 import pandas as pd
 
-# URL dos Exoplanetas da NASA
 START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
 
-# Webdriver
-browser = webdriver.Chrome("D:/Setup/chromedriver_win32/chromedriver.exe")
+browser = webdriver.Chrome("C:/Users/Guilh/OneDrive/Área de Trabalho/Projetos/Aula Byjus/chromedriver.exe")
 browser.get(START_URL)
 
 time.sleep(10)
@@ -18,44 +16,49 @@ new_planets_data = []
 
 def scrape_more_data(hyperlink):
     print(hyperlink)
-    
-    ## ADICIONE O CÓDIGO AQUI ##
+    try:
+        page=requests.get(hyperlink)
+        soap=BeautifulSoup(page.content, "html.parser")
+        temp_list=[]
+        for tr_tag in soap.find_all("tr", attrs={
+            "class":"fact_row"
+        }):
+            td_tags=tr_tag.find_all("td")
+            for td_tag in td_tags:
+                try: 
+                    temp_list.append(td_tag.find_all("div", attrs={
+                        "class":"value"
+                    })[0].contents[0])
+                except:
+                    temp_list.append("")
+        new_planets_data.append(temp_list)
+    except:
+        time.sleep(1)
+        scrape_more_data(hyperlink)
 
+planet_df_1 = pd.read_csv("C:/Users/Guilh/OneDrive/Área de Trabalho/PRO_1-1_C128_AtividadeDoAluno1/updated_scraped_data.csv")
 
-
-
-
-
-
-planet_df_1 = pd.read_csv("updated_scraped_data.csv")
-
-# Chame o método
 for index, row in planet_df_1.iterrows():
-
-    ## ADICIONE O CÓDIGO AQUI ##
-
-     # Call scrape_more_data(<hyperlink>)
-
-    print(f"Coleta de dados do hyperlink {index+1} concluída")
+    print(row["hyperlink"])
+    scrape_more_data(row["hyperlink"])
+    print("Colocando mais dados em Hyperlink", index+1)
 
 print(new_planets_data)
 
-# Remova o caractere '\n' dos dados coletados
 scraped_data = []
 
 for row in new_planets_data:
     replaced = []
-    ## ADICIONE O CÓDIGO AQUI ##
+    for el in row:
+        el=el.replace("\n", "")
+        replaced.append(el)
 
-
-    
     scraped_data.append(replaced)
 
 print(scraped_data)
 
 headers = ["planet_type","discovery_date", "mass", "planet_radius", "orbital_radius", "orbital_period", "eccentricity", "detection_method"]
 
-new_planet_df_1 = pd.DataFrame(scrapped_data,columns = headers)
+new_planet_df_1 = pd.DataFrame(scraped_data,columns = headers)
 
-# Converta para CSV
 new_planet_df_1.to_csv('new_scraped_data.csv', index=True, index_label="id")
